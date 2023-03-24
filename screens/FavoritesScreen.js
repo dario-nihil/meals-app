0eee80f2c7e1f5dcfbd6c3db65882654d4352f7a
dario-nihil <dario.nihil@gmail.com>
2023-03-24 09:02:56 +0100

    little change

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const FavoritesScreen = () => {
   return (
     <View>
-      <Text>The Favorite Component Screen</Text>
+      <Text>The Favorite Screen</Text>
     </View>
   );
 };
